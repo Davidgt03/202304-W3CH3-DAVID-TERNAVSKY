14e4325a652f2b548048b1d0c5cb558c476cd49d
david <daviidgt03@gmail.com>
2023-05-17 20:48:26 +0200

    Add display�

---
+++ src/models/pets.ts
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+/* export type PetsStructure = {
+  kind: string;
+  name: string;
+  age: string;
+};
+
+export class Pets implements PetsStructure {
+  static generatePet() {
+    return Math.trunc(Math.random() * 1_000_000_000_000).toString();
+  }
+
+  kind: string;
+  constructor(public title: string, public owner: string) {
+    this.kind = Pets.generatePet();
+  }
+}
+*/
